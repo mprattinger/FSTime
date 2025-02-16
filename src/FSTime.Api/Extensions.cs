8d2aa0b0d323956e69532f7ff5e55cbd16b516d3
Michael Prattinger <mprattinger@outlook.com>
2025-02-16 23:13:37 +0100

    wip

---
+++ src/FSTime.Api/Extensions.cs
@@ -0,0 +1,40 @@
+﻿namespace FSTime.Api;
+
+public static class Extensions
+{
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtConf = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+        if (jwtConf is not null)
+        {
+            services.AddSingleton(jwtConf);
+        }
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtConf?.Issuer,
+                    ValidAudience = jwtConf?.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtConf?.Secret!)),
+
+                };
+                options.MapInboundClaims = false;
+            });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
+
+record JwtSettings(string? Secret, int? ExpiryMinutes, string? Issure, string Audience)
+{
+    public const string SectionName = "JwtSettings";
+}
